b46256babb77b7a7c05eec677c4faa62b830025b
Marcin <borowiak04@gmail.com>
2023-11-29 11:22:58 +0100

    Version1.1

--- song.py
+++ song.py
@@ -4,23 +4,41 @@ class Song:
     is_played=None
     is_paused=None
     def __init__(self,url):
-        self.is_played=False
-        self.is_paused=False
+        self.__is_played=False
+        self.__is_paused=False
         self.url=url
         
+    def set_played(self,temp):
+        self.__is_played=temp
+        
+    def set_paused(self,temp):
+        self.__is_paused=temp
+        
+    def get_played(self):
+        x=self.__is_played
+        return x
 
+    def get_paused(self):
+        x=self.__is_paused
+        return x
     
     def play_song(self):
         mixer.init()
         mixer.music.load(self.url)
         mixer.music.play()
-        self.is_played=True
+        self.set_played(True)
+        #self.is_played=True
 
     def check_if_finished(self):
-        if mixer.music.get_busy() == True or self.is_paused==True:
-            self.is_played=True
+        # if mixer.music.get_busy() == True or self.is_paused==True:
+        #     self.is_played=True
+        # else:
+        #     self.is_played=False
+        temp=self.get_paused()
+        if mixer.music.get_busy() == True or temp==True:
+            self.set_played(True)
         else:
-            self.is_played=False
+            self.set_played(False)
         
         
 
@@ -28,15 +46,18 @@ class Song:
         mixer.music.set_volume(volumee)
         print("Ustawiono wartość dzwięku na:{}".format(volumee))
 
-    def pause_unpause_song(self): 
-        if self.is_played==True and self.is_paused==False:
+    def pause_unpause_song(self):
+        temp1=self.get_played()
+        temp2=self.get_paused()
+        if temp1==True and temp2==False:
             mixer.music.pause()
-            self.is_paused=True
+            #self.is_paused=True
+            self.set_paused(True)
             print("Zpauzowano piosenke")
-        elif self.is_played==True and self.is_paused==True:
+        elif temp1==True and temp2==True:
             mixer.music.unpause()
             time.sleep(1)
-            self.is_paused=False
+            self.set_paused(False)
             
             print("Odpauzowano ppiosenke")
         
@@ -44,9 +65,13 @@ class Song:
 
     def stop_song(self):
         self.check_if_finished()
-        if self.is_played == True:
+        # if self.is_played == True:
+        #     mixer.music.stop()
+        #     self.is_played=False
+        temp=self.get_played()
+        if temp == True:
             mixer.music.stop()
-            self.is_played=False
+            self.set_played(False)
         
 
 
@@ -56,14 +81,17 @@ class Song:
         else:
             next_song_url=current_queue.get_from_queue()
             print(next_song_url)
-            self.is_played=False
+            #self.is_played=False
+            self.set_played(False)
             time.sleep(0.5)
             self.stop_song()
             
             new_song=Song(next_song_url)
             new_song.play_song()
-            self.is_played=True
-            self.is_paused=False
+            # self.is_played=True
+            # self.is_paused=False
+            self.set_played(True)
+            self.set_paused(False)
             
 
     def queue_song(self,current_queue):
@@ -74,7 +102,13 @@ class Song:
             mixer.music.queue(next_song_url)
 
     def check_if_song_finished(self):
-        if mixer.music.get_busy() == False and self.is_played==True and self.is_paused==False:
+        temp1=self.get_played()
+        temp2=self.get_paused()
+        # if mixer.music.get_busy() == False and self.is_played==True and self.is_paused==False:
+        #     return True
+        # else:
+        #     return False
+        if mixer.music.get_busy() == False and temp1==True and temp2==False:
             return True
         else:
             return False
